543de1c6f99df9230316ee8e81b2f6f2dd11afdd
KataCalc <olegsen1n@ya.ru>
2022-10-18 19:29:52 +0300

    PP_2.2.1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,58 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_id")
+    private Long car_id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return car_id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + car_id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
